d370c310b31a65495b48cda28bee019dea9475a0
tripathiakankshaema <akanksha@ema.co>
2024-03-29 18:43:47 +0530

    removed desktopGraphLeftSection

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -183,18 +183,6 @@ export interface DataData extends Schema.Component {
   };
 }
 
-export interface DesktopGraphLeftSectionDesktopGraphLeftSection
-  extends Schema.Component {
-  collectionName: 'components_desktop_graph_left_section_desktop_graph_left_sections';
-  info: {
-    displayName: 'desktopGraphLeftSection';
-  };
-  attributes: {
-    header: Attribute.Text;
-    description: Attribute.Text;
-  };
-}
-
 export interface FaqListFaqList extends Schema.Component {
   collectionName: 'components_faq_list_faq_lists';
   info: {
@@ -675,7 +663,6 @@ declare module '@strapi/types' {
       'cust-strs-dt-list.cust-strs-dt-list': CustStrsDtListCustStrsDtList;
       'customer-list.customer-list': CustomerListCustomerList;
       'data.data': DataData;
-      'desktop-graph-left-section.desktop-graph-left-section': DesktopGraphLeftSectionDesktopGraphLeftSection;
       'faq-list.faq-list': FaqListFaqList;
       'footer-head.footer-head': FooterHeadFooterHead;
       'footer-section-desktop1.footer-section-desktop1': FooterSectionDesktop1FooterSectionDesktop1;
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1268,10 +1268,6 @@ export interface ApiHomeHome extends Schema.SingleType {
       'lowest-circle-badge-image.lowest-circle-badge-image',
       true
     >;
-    desktopGraphLeftSection: Attribute.Component<
-      'desktop-graph-left-section.desktop-graph-left-section',
-      true
-    >;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
